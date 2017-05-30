00cc383eb8bddd7589c8374a997bb5c5d618234d
franciscomb14 <javiermb.desarrollo@gmail.com>
2017-05-30 19:28:45 +0200

    Updated main with current matching feature

--- main.R
+++ main.R
@@ -1,29 +1,45 @@
-install.packages("ngram")
-install.packages("stringdist")
+#install.packages("ngram")
+#install.packages("stringdist")
+#install.packages("dplyr")
 library("ngram")
 library("stringdist")
+library("dplyr")
 
 rootDir <- "~/workspace/gitrepos/repos/PracticaDDS"
 
 # Load scripts
 source(paste(rootDir,"input/dataloader.R",sep="/"))
 source(paste(rootDir,"comparar.R",sep="/"))
+source(paste(rootDir,"matchingCPE.R",sep="/"))
+source(paste(rootDir,"lookupCPEByApplication.R",sep="/"))
+source(paste(rootDir,"doMatchingApplicationsCPE.R",sep = "/"))
 
-INPUT_COMPUTER_DIR <- paste(rootDir,"samples/input/computers",sep="/")
+INPUT_COMPUTER_DIR <- paste(rootDir,"samples/input/computers/one",sep="/")
 INPUT_SECURITY_DIR <- "input"
 
-computers.entries <- loadComputerSoftware(INPUT_COMPUTER_DIR)
+#0 Load official cve/cpes
 
-downloadSysdata(INPUT_SECURITY_DIR)
-load("~/workspace/gitrepos/repos/PracticaDDS/input/sysdata.rda")
+##downloadSysdata(INPUT_SECURITY_DIR)
+##load("~/workspace/gitrepos/repos/PracticaDDS/input/sysdata.rda")
 
-#Valores reales a usar
+# Load all CVEs/CPEs. Take care of that, a lot of data is loaded
 #cves <- netsec.data$datasets$cves
-#cpes <- netsec.data$datasets$cpes
-#Solo obtener 1000 valores para rapidez en calculos (solo de prueba)
-cpes <- net.security::GetDataFrame("cpes") 
+cpes <- netsec.data$datasets$cpes
+
+# Only load a sample from all CVEs/CPEs.
+#cpes <- net.security::GetDataFrame("cpes") 
 cves <- net.security::GetDataFrame("cves")
 
-#test de funcion en datos a comparar
-test <- lapply(computers.entries$Name, function(x) comparar(x, cpes$title))
+cpes <- head(cpes,n=50)
+
+#1 Collect data from PCs
+computers.entries <- loadComputerSoftware(INPUT_COMPUTER_DIR)
+names(computers.entries) <- c("name","version","vendor","computer") 
+
+# OPTIONAL: Work with a small sample
+# computers.entries <- head(computers.entries,4) 
+
+#2 Matching PC applications with CPE
+computers.entries.cpes <- doMatchingApplicationsCPE(computers.entries,cpes)
+
 
