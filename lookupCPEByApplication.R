658f7288f0b6dcaaa1142ac246809b3f05484d48
franciscomb14 <javiermb.desarrollo@gmail.com>
2017-05-30 19:28:12 +0200

    Resolved warining with factors

--- lookupCPEByApplication.R
+++ lookupCPEByApplication.R
@@ -9,6 +9,8 @@ lookupCPEByApplication <- function(applications.name) {
   #applications.name <- computers.entries$Name
   applications.name.ds <- as.data.frame.array(x=t(t(applications.name)))
   names(applications.name.ds) <- c("name")
-  return (applications.name.ds.distinct <- applications.name.ds %>% group_by(name) %>% summarise())
+  out <- applications.name.ds.distinct <- applications.name.ds %>% group_by(name) %>% summarise()
+  out$name <- as.character(out$name)
+  return (out)
 
 }
