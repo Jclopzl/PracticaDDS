10deb7a047eb0869de2e6a16b1dff52ee72dee89
franciscomb14 <javiermb.desarrollo@gmail.com>
2017-06-07 00:18:38 +0200

    Update match cve function

--- MatchCpeCve.R
+++ MatchCpeCve.R
@@ -1,6 +1,6 @@
-cves.sample <- tail(head(cves,n=3),n=10)
+cves.sample <- tail(head(cves,n=30000),n=10000)
 
-
+computers.entries.cpes <- matchingCPE.ds
 cves.sample.prejoin <- apply(cves.sample,1,function(cve.sample){
   
   cve.sample.ds=data.frame(as.list(cve.sample['cpe.software']),stringsAsFactors = FALSE)
@@ -18,7 +18,12 @@ cves.sample.prejoin <- apply(cves.sample,1,function(cve.sample){
     valTemp <- gsub(pattern = "\\]",replacement = "",x = valTemp)
     valTemp <- gsub(pattern = '"',replacement = "",x = valTemp)
     valTemp <- gsub(pattern = "\\\\",replacement = "",x = valTemp)
-    valTemp <- cleanCpe(valTemp)
+    valTemp <- gsub(pattern = "/",replacement = "",x = valTemp)
+    valTemp <- gsub(pattern = "\\{",replacement = "",x = valTemp)
+    valTemp <- gsub(pattern = "\\}",replacement = "",x = valTemp)
+    valTemp <- gsub(pattern = "%2",replacement = ".",x = valTemp)
+    #valTemp <- cleanCpe(valTemp)
+    
     
     
     return(paste(cve.sample.cve,valTemp,sep='|'))
@@ -38,9 +43,36 @@ cves.sample.prejoin <- apply(cves.sample,1,function(cve.sample){
 # convert to a dataframe
 cves.sample.prejoin.ds <- t(data.frame(cves.sample.prejoin))
 cves.sample.prejoin.ds <- data.frame(cves.sample.prejoin.ds,stringsAsFactors = FALSE)
+
 names(cves.sample.prejoin.ds) <- c("cve")
 
 # Separate into two columns: cve and cpe found
 cves.sample.prejoin.ds <- cves.sample.prejoin.ds %>% tidyr::separate(cve,into=c("cve","cpe"),sep = "\\|")
 
-res <- left_join(computers.entries.cpes, cves.sample.prejoin.ds, by=("cpe"))
+computers.entries.cves <- left_join(computers.entries.cpes, cves.sample.prejoin.ds, by=("cpe"))
+computers.entries.cves.nona <- filter(computers.entries.cves,!is.na(cve))
+computers.entries.cves.na <- filter(computers.entries.cves,is.na(cve))
+
+# Add cvss information
+computers.entries.cves.nona.complete <- left_join(computers.entries.cves.nona, cves, by=("cve"))
+
+# Select required columns
+computers.entries.cves.nona.final <- select(computers.entries.cves.nona.complete, name,version,vendor,computer,cve,cvss)
+
+# Retrieve cvss value
+computers.entries.cves.nona.final$cvss <- sapply(computers.entries.cves.nona.final$cvss, function(cvss.json) { 
+  value <- strsplit(strsplit(strsplit(cvss.json,split="score")[[1]][2],split="\\]")[[1]][1],split="\\[")[[1]][2]
+  value <- gsub(pattern = '"',replacement = "",x = value)
+  return(as.numeric(value)) })
+
+# Complete computers with no CVE
+computers.entries.cves.na$cve=replicate(nrow(computers.entries.cves.na),NA)
+computers.entries.cves.na$cvss=replicate(nrow(computers.entries.cves.na),0)
+computers.entries.cves.na.final <- select(computers.entries.cves.na, name,version,vendor,computer,cve,cvss)
+
+# Join computers with cves and computers with no cvs
+computers.entries.cves.final <- dplyr::bind_rows(computers.entries.cves.na.final,computers.entries.cves.nona.final)
+
+
+ 
+ 
