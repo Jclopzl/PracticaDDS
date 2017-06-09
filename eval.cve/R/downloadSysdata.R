2f2553038cb5a04f3992b74beae2d04da42eabf1
Juan Carlos Lopez <jclopzl@la-MacBook-Air-de-Juan.local>
2017-06-09 17:05:18 +0200

    Erase of the eval.cpe package

--- eval.cve/R/downloadSysdata.R
+++
@@ -1,8 +0,0 @@
-downloadSysdata <-
-function(input.dir) {
-  
-  url <- "https://github.com/r-net-tools/security.datasets/raw/master/net.security/sysdata.rda"
-  path <- paste(input.dir,"sysdata.rda",sep = "/")
-  download.file(url = url, path)
-  
-}
